311a2c457debd283bb35bde4edc9629306e828f4
Anouk Borris <anoukborris@gmail.com>
2022-12-08 04:29:52 +0000

    react attempts

--- biddr_client/biddr_client/src/App.js
+++ biddr_client/biddr_client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import AuctionIndexPage from "./components/AuctionIndexPage";
+import AuctionShowPage from "./components/AuctionShowPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import NavBar from "./components/NavBar"
+import SignInPage from "./components/SignInPage";
 
-function App() {
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+          <Route
+            exact
+            path="/auctions"
+            element={<AuctionIndexPage/>}
+          />
+          <Route
+            path="/"
+            element={<WelcomePage/>}
+            exact
+          />
+          <Route
+            path="/"
+            element={<SignInPage/>}
+            exact
+          />
+          <Route
+            exact
+            path="/auctions/:id"
+            render={(routeProps) => (
+              <AuctionShowPage {...routeProps}/>
+            )}
+          />
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ biddr_client/biddr_client/src/components/auctionData.js
@@ -0,0 +1,17 @@
+
+
+function auctionData() {
+    const auction = {
+        id: 102,
+        title: 'Snowboard Boots X-L',
+        description: 'A great pair of boots, hardly used',
+        created_at: '2014-June-25',
+        seller: {
+          full_name: 'Jimbo McStreme'
+        }
+        
+        
+      };
+      return auction;
+}
+export default auctionData
